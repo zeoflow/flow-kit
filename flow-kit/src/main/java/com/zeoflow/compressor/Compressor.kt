b013ee3925525fe9005cc83e19f7bf71e7974aaa
Teodor G <teo@zeoflow.com>
2021-03-13 23:05:43 +0200

    variant name changed

--- flow-kit/src/main/java/com/zeoflow/compressor/Compressor.kt
+++ flow-kit/src/main/java/com/zeoflow/compressor/Compressor.kt
@@ -11,7 +11,7 @@ import kotlin.coroutines.CoroutineContext
 object Compressor {
 
     @JvmStatic
-    fun variantJava(): JavaCompressor {
+    fun javaVariant(): JavaCompressor {
         return JavaCompressor()
     }
 
