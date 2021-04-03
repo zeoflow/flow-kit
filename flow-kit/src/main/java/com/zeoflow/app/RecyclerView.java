c3e83c573fbb3356789294908b5a85e2635b0619
Teodor G <teo@zeoflow.com>
2021-04-03 19:14:38 +0300

    removed unwanted data

--- flow-kit/src/main/java/com/zeoflow/app/RecyclerView.java
+++ flow-kit/src/main/java/com/zeoflow/app/RecyclerView.java
@@ -17,15 +17,12 @@ package com.zeoflow.app;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.zeoflow.annotation.NonNull;
 import com.zeoflow.annotation.Nullable;
 import com.zeoflow.initializer.ZeoFlowApp;
 import com.zeoflow.log.Log;
 import com.zeoflow.zson.Zson;
-
-import java.util.List;
 
 public abstract class RecyclerView
 {
@@ -87,84 +84,6 @@ public abstract class RecyclerView
             super();
             Log.configure(log_tag);
         }
-        @androidx.annotation.NonNull
-        @Override
-        public androidx.recyclerview.widget.RecyclerView.ViewHolder onCreateViewHolder(@androidx.annotation.NonNull ViewGroup parent, int viewType)
-        {
-            return null;
-        }
-        @Override
-        public void onBindViewHolder(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder holder, int position, @androidx.annotation.NonNull List<Object> payloads)
-        {
-            super.onBindViewHolder(holder, position, payloads);
-        }
-        @Override
-        public int getItemViewType(int position)
-        {
-            return super.getItemViewType(position);
-        }
-        @Override
-        public void setHasStableIds(boolean hasStableIds)
-        {
-            super.setHasStableIds(hasStableIds);
-        }
-        @Override
-        public long getItemId(int position)
-        {
-            return super.getItemId(position);
-        }
-        @Override
-        public void onViewRecycled(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder holder)
-        {
-            super.onViewRecycled(holder);
-        }
-        @Override
-        public boolean onFailedToRecycleView(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder holder)
-        {
-            return super.onFailedToRecycleView(holder);
-        }
-        @Override
-        public void onViewAttachedToWindow(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder holder)
-        {
-            super.onViewAttachedToWindow(holder);
-        }
-        @Override
-        public void onViewDetachedFromWindow(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder holder)
-        {
-            super.onViewDetachedFromWindow(holder);
-        }
-        @Override
-        public void registerAdapterDataObserver(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView.AdapterDataObserver observer)
-        {
-            super.registerAdapterDataObserver(observer);
-        }
-        @Override
-        public void unregisterAdapterDataObserver(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView.AdapterDataObserver observer)
-        {
-            super.unregisterAdapterDataObserver(observer);
-        }
-        @Override
-        public void onAttachedToRecyclerView(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView recyclerView)
-        {
-            super.onAttachedToRecyclerView(recyclerView);
-        }
-        @Override
-        public void onDetachedFromRecyclerView(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView recyclerView)
-        {
-            super.onDetachedFromRecyclerView(recyclerView);
-        }
-        @Override
-        public void onBindViewHolder(@androidx.annotation.NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder holder, int position)
-        {
-
-        }
-        @Override
-        public int getItemCount()
-        {
-            return 0;
-        }
-
-        // custom data
         public void withLoggerTag(@NonNull String tag)
         {
             log_tag = tag;
