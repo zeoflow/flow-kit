ba27ca9887a23ac557d6f36be113914c9cecbf34
Teodor G <teo@zeoflow.com>
2021-04-10 00:05:30 +0300

    example added

--- catalog/src/main/java/com/zeoflow/sample/MainActivity.java
+++ catalog/src/main/java/com/zeoflow/sample/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 
 import com.zeoflow.app.Activity;
+import com.zeoflow.utils.string.StringCreator;
 
 public class MainActivity extends Activity
 {
@@ -14,6 +15,19 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StringCreator string = StringCreator.creator()
+                .add("double strings $S$S", String.class, MainActivity.class)
+                .add("upper string multiple 's' $S:Ssss", String.class)
+                .add("upper string $S:S's", String.class)
+                .add("lower string $S:s", String.class)
+                .add("name $N", "String.class")
+                .add("upper name $N:S's", "String.class")
+                .add("lower name $N:s's", "String.class")
+                .add("class full package $C", String.class)
+                .add("class name only $c", String.class)
+                .create();
+        log(string.asString());
     }
 
 }
